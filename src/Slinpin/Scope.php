1d0dac357266571ef9da142b053c88f5fb38fd63
Terrence Howard <chemisus@gmail.com>
2012-11-30 15:43:18 -0500

    added scope, inject, invoke, and factory injectors

--- src/Slinpin/Scope.php
+++ src/Slinpin/Scope.php
@@ -15,6 +15,28 @@ class Scope extends Container {
         return $value;
     }
     
+    public function __construct() {
+        $this->set('scope', $this->constant($this));
+
+        $this->set('invoke', function ($scope) {
+            return function ($key, $values=array()) use ($scope) {
+                return $scope->invoke($key, $values);
+            };
+        });
+        
+        $this->set('inject', function ($scope) {
+            return function ($value, $values=array(), $keys=null) use ($scope) {
+                return $scope->method($value, $values, $keys);
+            };
+        });
+        
+        $this->set('instance', function ($scope) {
+            return function ($value, $values=array(), $keys=null) use ($scope) {
+                return $scope->factory($value, $values, $keys);
+            };
+        });
+    }
+    
     public function invoker($value, $values=array(), $keys=null) {
         return new Method(
             new Injector(new Container($values),
